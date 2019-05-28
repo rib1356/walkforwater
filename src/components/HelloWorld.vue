1d501740f01b4db60cb83a7de6f49e84f8fdeb0e
Rib1356 <rib1356@hotmail.co.uk>
2019-05-28 17:01:43 +0100

    mroe changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <div class="welcome">
-      <button @click="top" id="myBtn" title="Go to top" class="myBtn">Back To Top</button>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <!-- <button @click="top" id="myBtn" title="Go to top" class="myBtn">Back To Top</button> -->
+    <i class="fas fa-arrow-circle-up fa-2x myBtn" id="myBtn" @click="top"></i>
+    <div class="child-div">
       <div> <!-- Navigation -->
         <p>Click to navigate the page</p>
         <button @click="whyWalk" class="myButton">Why Walk?</button>
@@ -19,18 +21,20 @@
          The walk, which will span eight days, will raise funds for the Rotary-supported charity Village Water, (www.villagewater.org), which provides safe water, sanitation and hygiene for, and fosters independence among, African communities and accounts for every penny! </p>
       <hr>   
     </div>
-    <!-- <div class="why-walk">
+    <!---WHY WALK----------------------------------------------------------------------------------------------------------------------------------------->
+    <div class="child-div" id="whyWalk">
       <h2>Why Walk?</h2>
       <div class="why-walk-p">
         <p>Because apart from enjoying the marvellous scenery and keeping healthy, you’ll be helping to transform the lives of villagers in Zambia’s Western Province where almost half the population still lacks safe water. Village Water’s ‘One Village At A Time’ campaign is hoping to change this.</p>
         <p>Here the scarcity of safe water means eye and infections are prevalent as is diarrhoea, in fact it kills 10 children under five years of age every day in Zambia! People often have to walk miles to a safe water source – so every step you take will remind you of their daily drudge and the reason to keep on walking!</p>
         <p>The funds raised will help supply wells so that villagers can grow their own vegetables and bananas for themselves and sell to raise income. The community will get the education and support they need to take charge of their own hygiene and sanitation, and a protected waterpoint and training to maintain it. So lasting safe water is close to home whenever they need it.</p>
-        <img src="@/assets/z1.png" alt="logo" height="200" >
+        <img src="@/assets/z1.png" alt="logo" height="400">
         <img src="@/assets/51.png" alt="logo" height="200" width="200">
       </div>
       <hr>
-    </div> -->
-    <!-- <div class="the-walk">
+    </div>
+    <!--- THE WALK ----------------------------------------------------------------------------------------------------------------------------------------->
+    <div class="child-div" id="theWalk">
       <h2>The Walk</h2>
       <h4>You can do it!</h4>
       <p>Those who may feel daunted by an eight day trek, can still join in because we’ve subdivided the route into manageable stages of 5-14 miles per day. You can join us for half a day, a full day or stay with us every step of the 100-mile way!</p>
@@ -38,74 +42,66 @@
          Whilst the walk leader and Rotary members will be there to guide and support you, your personal health and safety is your own responsibility. </p>
       <h5>Below you'll find the route and stages:</h5>   
       <img src="@/assets/map.png">
-      <table>
-        <tr>
-          <th>Section</th>
-          <th>What to expect</th>
-          <th>First section</th>
-          <th>Second section</th>
-        </tr>
-        <tr>
-          <th>Day 1: Sunday 8th September </th>
-          <th>A long, strenuous climb of 420m over 4miles to High Cup Nick. You'll encounter a number of fords before a rocky path down to to Cauldron Snout. There's some rocky paths on the way to Widdy Bank farm before an easier final section. Excellent views.</th>
-          <th>9.6 miles Strenuous</th>
-          <th>4.4 miles Strenuous </th>
-        </tr>
-        <tr>
-          <th>Day 2: Monday 9th September</th>
-          <th>A day of scenic views, passing High and Low Force, then through meadowland to Middleton in Teesdale</th>
-          <th>5.4 miles Moderate</th>
-          <th>3.6 miles Easy</th>
-        </tr>
-        <tr>
-          <th>Day 3: Tuesday 10th September</th>
-          <th>A day of picture postcard views passing through Romaldkirk to Barnard Castle</th>
-          <th>6.2 miles Moderate</th>
-          <th>6.8 miles Moderate</th>
-        </tr>
-        <tr>
-          <th>Day 4: Wednesday 11th September</th>
-          <th>A day of historic sites from Barnard Castle, past Egglestone Abbey, skirting Rokeby Park then to Whorlton Bridge and on to Gainford.</th>
-          <th>4.5 miles Moderate</th>
-          <th>5.9 miles Moderate</th>
-        </tr>
-        <tr>
-          <th>Day 5: Thursday 12th September</th>
-          <th>The path now starts to meander through farmland which can be muddy. Once we reach Broken Scar the route skirts close to Darlington before going through Croft to Hurworth.</th>
-          <th>8.4 miles Moderate</th>
-          <th>7.1 miles Moderate</th>
-        </tr>
-        <tr>
-          <th>Day 6: Friday 13th September</th>
-          <th>After following a minor road from Hurworth to Neasham we return to a footpath by the river. Whilst fairly easy walking this path is not well maintained and can be muddy.</th>
-          <th>6.6 miles Moderate</th>
-          <th>7.6 miles Moderate</th>
-        </tr>
-        <tr>
-          <th>Day 7: Saturday 14th September</th>
-          <th>From Yarm we enter the more residential part of the route but still with good open spaces, and the added interest of the Tees Barrage and the Transporter Bridge.</th>
-          <th>8.1 miles Moderate</th>
-          <th>5.3 miles Easy</th>
-        </tr>
-        <tr>
-          <th>Day 8: Sunday 15th September</th>
-          <th>The day's first stage mainly follows the disused railway line through Middlesbrough's industrial heartland. We celebrate the end of our walk with a stretch from Redcar to South Gare and the mouth of the Tees.</th>
-          <th>7.4 miles Easy</th>
-          <th>3.1 miles Easy</th>
-        </tr>
-        <tfoot>
-          <th colspan="4">
-          Important note: As Giant Hogweed grows in a number of places along the Tees, especially from Day 5 to the first stage of day 7,  you are advised to wear long trousers.
-          </th>
-        </tfoot>
-      </table>
+      <b-table stacked="md" striped :items="items">
+      </b-table>
+      <h5>
+        Important note: As Giant Hogweed grows in a number of places along the Tees, especially from Day 5 to the first stage of day 7,  you are advised to wear long trousers.
+      </h5>  
         <p>
-        <img src="@/assets/tees1.jpg" alt="logo" height="250" width="300">
+        <img src="@/assets/tees1.jpg" alt="logo" height="400" width="300">
         <img src="@/assets/z2.png" alt="logo" height="250" width="300" >
         </p>
         <hr>
     </div>
-    <div class="our-links">
+    <!--- INFORMATION ----------------------------------------------------------------------------------------------------------------------------------------->
+    <div class="child-div" id="information">
+      <h2>Information</h2>
+      <div>
+        <h5><b>All funds raised will go to Village Waters one Village at a Time Project in Zambia.</b></h5>
+        <h5><b>You will first need to register for the walk.</b></h5>
+        <p>The registration fee is adults £10.00 per day up to £30 maximum.</p>
+        <p>Young people under the age of 16 years £5.00</p>
+        <p class="bacs"><i>Please register and pay by BACS online</i></p>
+        <p class="bacs"><i>Sort code: 20-56-74</i></p>
+        <p class="bacs"><i>Account number: 10435287</i></p>
+        <p class="bacs"><i>Please include your surname and initials as reference eg w4w Blogs J</i></p>
+        <p>Your information pack, including Registration and Sponsorship forms, and can be obtained from walk4waterfund@gmail.com</p>
+        <p>The start points and times for each day and half day sections are clearly identified in your information pack but please be aware, getting to and from the start and finish points is your own responsibility so forward-planning is key. Marshals will
+           greet you at the start points and you’ll find post codes/map reference points on the mileage chart.</p>
+        <br>
+      </div>
+      <p><b>Disabled access: </b>Limited availability for disabled access.</p>
+      <p><b>Where you can join us on the walk: </b>The start points for each day and half day sections are within this pack.</p>
+      <p><b>Getting to and from the walk is your responsibility: </b>Official places and departure times showing where you can join the walk for a section, a day or several days is clearly marked on the Walk Sections sheet. Please be at your start point 30 minutes prior to your start time. There will be Marshals to greet you at these points. Post codes/map reference points are available on the mileage chart.</p>
+      <p><b>BEFORE WE GO - PREPARING FOR THE WALK</b></p>
+      <div class="parent">
+        <p>There’s lots to think about before we set off so here’s some helpful tips to get you planning</p>
+        <ul>
+          <li>The terrain is rugged for the first four days, so we advise good walking boots for these sections.</li>
+          <li>Don’t forget some drinking water.</li>
+          <li>Wear well-fitting comfortable boots or sturdy trail shoes depending on the weather.</li>
+          <li>Don’t underestimate the weather, especially extremes of wet and wind.</li>
+          <li>Dress in layers: thin thermals are good for base layers; woollen jumpers and/or mid weight polyester fleeces will suffice for mid-layers. <br/> While a waterproof jacket is essential to keep the wind and rain at bay.</li>
+          <li>Opt for light, flexible, quick drying trousers or shorts if the weather is hot. Wear a hat if it’s sunny – and a different one if it’s cold!</li>
+        </ul>
+        <p>You may also like to take</p>
+        <ul>
+          <li>A basic first aid kit</li>
+          <li>Camera</li>
+          <li>Sunglasses</li>
+          <li>Binoculars</li>
+          <li>Mobile Phone</li>
+          <li>Walking stick</li>
+          <li>Pre-packed refreshments</li>
+          <li>Your own map - though Rotary guides will have maps</li>
+          <li>And, of course, <b>Dont forget sunscreen!</b></li>
+        </ul>
+        <h5>WANT TO KNOW MORE OR TALK IT OVER</h5>
+        <p>If you want to know anything specific, we’d be happy to help. Please call 01642 714562 or email us on walkforwaterfund@gmail.com</p>
+        <p><b>We welcome young people to participate: </b>Young people under the age of 16 must always have a responsible adult to walk with them.</p>
+      </div>  
+    </div>
+    <!-- <div class="our-links">
       <h2>Our links</h2>
       <ul>
         <li><a href="https://www.rotary-ribi.org/clubs/homepage.php?ClubID=166" target="_blank" rel="noopener">Stokesley Rotary Club</a></li>
@@ -118,21 +114,62 @@
 <script>
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+       items: [
+            {
+              date: "Day 1: Sunday 8th September", 
+              WhatToExpect: "A long, strenuous climb of 420m over 4miles to High Cup Nick. You'll encounter a number of fords before a rocky path down to to Cauldron Snout. There's some rocky paths on the way to Widdy Bank farm before an easier final section. Excellent views",
+              FirstSection: "9.6 miles Strenuous",
+              SecondSection: "4.4 miles Strenuous"},
+            {
+              date: "Day 2: Monday 9th September", 
+              WhatToExpect: "A day of scenic views, passing High and Low Force, then through meadowland to Middleton in Teesdale",
+              FirstSection: "5.4 miles Moderate",
+              SecondSection: "3.6 miles Easy"},
+            {
+              date: "Day 3: Tuesday 10th September", 
+              WhatToExpect: "A day of picture postcard views passing through Romaldkirk to Barnard Castle",
+              FirstSection: "6.2 miles Moderate",
+              SecondSection: "6.8 miles Moderate"},
+            {
+              date: "Day 4: Wednesday 11th September", 
+              WhatToExpect: "A day of historic sites from Barnard Castle, past Egglestone Abbey, skirting Rokeby Park then to Whorlton Bridge and on to Gainford",
+              FirstSection: "4.5 miles Moderate",
+              SecondSection: "5.9 miles Moderate"},
+            {
+              date: "Day 5: Thursday 12th September", 
+              WhatToExpect: "The path now starts to meander through farmland which can be muddy. Once we reach Broken Scar the route skirts close to Darlington before going through Croft to Hurworth",
+              FirstSection: "8.4 miles Moderate",
+              SecondSection: "7.1 miles Moderate"},
+            {
+              date: "Day 6: Friday 13th September", 
+              WhatToExpect: "After following a minor road from Hurworth to Neasham we return to a footpath by the river. Whilst fairly easy walking this path is not well maintained and can be muddy",
+              FirstSection: "6.6 miles Moderate",
+              SecondSection: "7.6 miles Moderate"},
+            {
+              date: "Day 7: Saturday 14th September", 
+              WhatToExpect: "From Yarm we enter the more residential part of the route but still with good open spaces, and the added interest of the Tees Barrage and the Transporter Bridge",
+              FirstSection: "8.1 miles Moderate",
+              SecondSection: "5.3 miles Easy"},
+            {
+              date: "Day 8: Sunday 15th September", 
+              WhatToExpect: "The day's first stage mainly follows the disused railway line through Middlesbrough's industrial heartland. We celebrate the end of our walk with a stretch from Redcar to South Gare and the mouth of the Tees",
+              FirstSection: "7.4 miles Easy",
+              SecondSection: "3.1 miles Easy"},
+        ],
+
+    }
+  },
   props: {
     msg: String,
-    items: [
-      {date: "Day 1: Sunday 8th September", 
-       example: "A long, strenuous climb of 420m over 4miles to High Cup Nick. You'll encounter a number of fords before a rocky path down to to Cauldron Snout. There's some rocky paths on the way to Widdy Bank farm before an easier final section. Excellent views.",
-       section1: "9.6 miles Strenuous",
-       section2: "4.4 miles Strenuous"},
-    ]
   },
   methods: {
     whyWalk(){
-      window.scrollTo(0,750);
+      document.getElementById('whyWalk').scrollIntoView();
     },
     theWalk(){
-      window.scrollTo(0,1420);
+      document.getElementById('theWalk').scrollIntoView();
     },
     top(){
       window.scrollTo(0,0);
@@ -159,28 +196,54 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://use.fontawesome.com/releases/v5.7.2/css/all.css";
 
   h3 {
     margin-top: 0px;
     margin-bottom: 0px;
   }
 
-  .welcome {
-    height: 730px;
+  .child-div {
+    height: 100%;
     width: 100%;
     background-color: white;
+  }
+  
+  .bacs {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+  .bacs {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  .parent {
+    margin: auto;
+    text-align: center;
+  }
+  .parent > ul {
+    display: inline-block;
+
+  }
+
+  /* .welcome {
+    height: 100%;
+    width: 100%;
+    background-color: white;
+    
   }
   .why-walk {
-    height: 650px;
+    height: 100%;
     width: 100%;
     background-color: white;
-  }
+  } */
   .why-walk-p {
     width: 80%;
     text-align: center;
     margin: auto;
   }
-  .the-walk {
+  /* .the-walk {
     height: 1300px;
     width: 100%;
     background-color: white;
@@ -189,32 +252,34 @@ export default {
     height: 800px;
     width: 100%;
     background-color: white;
-  }
+  } */
 
-  table {
+  /* table {
     font-family: arial, sans-serif;
     border-collapse: collapse;
     width: 80%;
     margin: auto;
-  }
+    -webkit-overflow-scrolling: touch;
+    overflow-x: auto;
+    display: block;
+  } */
 
-  td, th {
+  /* td, th {
     border: 1px solid #dddddd;
     text-align: left;
     padding: 8px;
-  }
-
+  } */
+/* 
   tr:nth-child(even) {
     background-color: #dddddd;
-  }
+  } */
 
   @media only screen and (max-width : 768px) {
 
-    table {
-      width: auto ;
+    /* table {
+      width: 100%;
       max-width: 100% ;
-      height: auto ;
-    }
+    } */
 
     img {
     width: auto ;
@@ -222,10 +287,8 @@ export default {
     height: auto ;
     }
 
-    p {
-      height: auto;
-      width: auto ;
-    max-width: 100% ;
+    .why-walk-p {
+      width: 100% ;
     }
 
   }
@@ -277,31 +340,10 @@ export default {
   bottom: 20px; /* Place the button at the bottom of the page */
   right: 30px; /* Place the button 30px from the right */
   z-index: 99; /* Make sure it does not overlap */
-  -moz-box-shadow:inset 0px 1px 0px 0px #8a2a21;
-	-webkit-box-shadow:inset 0px 1px 0px 0px #8a2a21;
-	box-shadow:inset 0px 1px 0px 0px #8a2a21;
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #c62d1f), color-stop(1, #f24437));
-	background:-moz-linear-gradient(top, #c62d1f 5%, #f24437 100%);
-	background:-webkit-linear-gradient(top, #c62d1f 5%, #f24437 100%);
-	background:-o-linear-gradient(top, #c62d1f 5%, #f24437 100%);
-	background:-ms-linear-gradient(top, #c62d1f 5%, #f24437 100%);
-	background:linear-gradient(to bottom, #c62d1f 5%, #f24437 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#c62d1f', endColorstr='#f24437',GradientType=0);
-	background-color:#c62d1f;
-	-moz-border-radius:6px;
-	-webkit-border-radius:6px;
-	border-radius:6px;
-	border:1px solid #d02718;
 	display:inline-block;
 	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:15px;
-	font-weight:bold;
-	padding:6px 24px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #810e05;
-  
+  color: red;
+  opacity: 0.7;
 }
 
 </style>
